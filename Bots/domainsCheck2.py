04ff0addd1f75b952cf4cd00e29c3793778b1ddb
Luiz H. S. Bispo <luizh3bispo@gmail.com>
2022-04-22 20:05:39 -0300

    update

--- Bots/domainsCheck2.py
+++ Bots/domainsCheck2.py
@@ -1,31 +1,35 @@
+from openpyxl import load_workbook
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 import time
-import openpyxl
 
 print("Initiating the bot...\n")
 
-'''deprecated mode
-PATH = 'C:/Users/Pc/Desktop/Luiz/Robos/chromedriver'
-driver = webdriver.Chrome(PATH)
-driver.get("https://registro.br/")
-'''
+domains = []
 
-#cleaner mode
+#sheet index
+n = 0
+
+#Reading from Excel file
+PATH = "C:/Users/Pc/Documents/GitHub/work_bot/Bots/ExcelFiles/Domains.xlsx"
+workbook = load_workbook(PATH)
+sheets = workbook.sheetnames
+wb = workbook[sheets[n]]
+for row in range(1,10):
+    domains.append(wb.cell(row,1).value)
+
+#webdriver
 ser = Service('C:/Users/Pc/Desktop/Luiz/Robos/chromedriver')
 op = webdriver.ChromeOptions()
 #option to remove bluetooth adapter error
-op.add_experimental_option("excludeSwitches", ["enable-logging"]) 
-
+op.add_experimental_option("excludeSwitches", ["enable-logging"])
 driver = webdriver.Chrome(service=ser, options=op)
-
 url = "https://registro.br/"
 driver.get(url)
 
 #domains list to check
-domains = ["roboscompython.com.br","udemy.com","google.com","aleia.com.br", "strauusss.com"]
 for domain in domains:
     search = driver.find_element(By.ID, "is-avail-field")
     search.clear()  # Cleaning the search bar
@@ -34,9 +38,8 @@ for domain in domains:
     time.sleep(2) #time to load the page
     #find information with a Tag
     results = driver.find_elements(By.TAG_NAME,"strong")
-    # method to stop the code and trace the results by results[i].text. (c + enter closes it)
-    #import pdb; pdb.set_trace()
     #print results
     print("Domain %s %s" % (domain, results[4].text))
 
 driver.close()
+
