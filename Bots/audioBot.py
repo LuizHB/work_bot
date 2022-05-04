9d775952c028340390ffea1a656951cbb458e0cf
Luiz H. S. Bispo <luizh3bispo@gmail.com>
2022-05-04 12:47:06 -0300

    update

--- Bots/audioBot.py
+++ Bots/audioBot.py
@@ -4,7 +4,7 @@ from gtts import gTTS
 from tkinter.filedialog import askopenfilename
 from tkinter import Tk
 Tk().withdraw()
-filelocation = askopenfilename(initialdir="C:\\Users\Pc\\Documents\\GitHub\\work_bot\\Bots\\Files", title="open file", filetypes=(("document", "*.pdf"),("document","*.*")))# Escolher arquivo com GUI
+filelocation = askopenfilename(initialdir="C:\\Users\Pc\\Documents\\GitHub\\work_bot\\Bots\\Files", title="open file", filetypes=(("document", "*.pdf"),("document","*.*")))# Choose the file
 
 book = open(filelocation, 'rb')
 print(book.read())
@@ -13,20 +13,20 @@ pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
 print("No. of pages: ", pages)
 speaker = pyttsx3.init()
-whole_text = '' # Armazena todos os textos
-choice = input('Percorrer até que página? ')
+whole_text = '' # Get the whole text
+choice = input('Go till which page? ')
 
 for num in range(0, int(choice)):
     page = pdfReader.getPage(num)
     text = page.extractText()
     whole_text += text
     speaker.say(text)
-    print("Lendo: ", text)
+    print("Reading: ", text)
     speaker.runAndWait()
 
 
 final_file = gTTS(text=whole_text, lang='pt')
-name = input("Digite o nome do texto a ser salvo: ")
-final_file.save( name+".mp3")  # Salvar arquivo mp3
+name = input("Type the name of the file to be saved: ")
+final_file.save( name+".mp3")  # Save file in mp3
 
 book.close()
