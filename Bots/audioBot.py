eff5b3e0c80a075ec8c49eaf4463be8ffc152ecb
Luiz H. S. Bispo <luizh3bispo@gmail.com>
2022-05-04 13:34:36 -0300

    update audiobot

--- Bots/audioBot.py
+++ Bots/audioBot.py
@@ -1,32 +1,28 @@
 import pyttsx3
-import PyPDF2
+import fitz,sys
 from gtts import gTTS
-from tkinter.filedialog import askopenfilename
 from tkinter import Tk
+
 Tk().withdraw()
-filelocation = askopenfilename(initialdir="C:\\Users\Pc\\Documents\\GitHub\\work_bot\\Bots\\Files", title="open file", filetypes=(("document", "*.pdf"),("document","*.*")))# Choose the file
+# Choose the file
+book = fitz.open(r'C:\Users\Pc\Documents\GitHub\work_bot\Bots\Files\Lorem_ipsum.pdf')
 
-book = open(filelocation, 'rb')
-print(book.read())
-
-pdfReader = PyPDF2.PdfFileReader(book)
-pages = pdfReader.numPages
+pages = book.page_count
 print("No. of pages: ", pages)
 speaker = pyttsx3.init()
-whole_text = '' # Get the whole text
+whole_text = ''  # Get the whole text
 choice = input('Go till which page? ')
 
 for num in range(0, int(choice)):
-    page = pdfReader.getPage(num)
-    text = page.extractText()
+    page = book.load_page(num)
+    text = page.get_text("text")
     whole_text += text
     speaker.say(text)
     print("Reading: ", text)
     speaker.runAndWait()
 
-
 final_file = gTTS(text=whole_text, lang='pt')
 name = input("Type the name of the file to be saved: ")
-final_file.save( name+".mp3")  # Save file in mp3
+final_file.save(name + ".mp3")  # Save file in mp3
 
 book.close()
